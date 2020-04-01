15e2af5ebd551a7332286020c73ff0c2df3583ce
Mark Stacy <mbstacy@gmail.com>
2020-04-01 15:47:11 -0600

    update pymongo and required packages

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='data-gatecountq',
-      version='0.0',
+      version='0.1',
       packages=find_packages(),
       install_requires=[
           'celery==3.1.22',
-          'pymongo==3.2.1',
+          'pymongo',
           'requests',
+          'python-dateutil',
       ],
       )
