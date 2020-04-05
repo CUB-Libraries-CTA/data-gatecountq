4cfb4839a12dd1a3d4af293a1958a6953e66787a
Mark Stacy <mbstacy@gmail.com>
2020-04-05 14:12:12 -0600

    Update celery version

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name='data-gatecountq',
-      version='0.2',
+      version='0.3',
       packages=find_packages(),
       install_requires=[
-          'celery',
+          'celery==4.3.0',
           'pymongo',
           'requests',
           'python-dateutil',
